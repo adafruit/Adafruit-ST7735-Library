bd3abf87b8ce2f754f0df3315135601fa9ba808b
ladyada <limor@ladyada.net>
2014-09-05 16:11:28 -0400

    updated rotation tester for default SPI and 1.44"

--- examples/rotationtest/rotationtest.ino
+++ examples/rotationtest/rotationtest.ino
@@ -16,56 +16,38 @@
   MIT license, all text above must be included in any redistribution
  ****************************************************/
 
-// For the breakout, you can use any (4 or) 5 pins
-//#define sclk 4
-//#define mosi 5
-//#define cs   6
-//#define dc   7
-//#define rst  8  // you can also connect this to the Arduino reset
-
-//Use these pins for the shield!
-#define sclk 13
-#define mosi 11
-#define cs   10
-#define dc   8
-#define rst  0  // you can also connect this to the Arduino reset
-
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7735.h> // Hardware-specific library
 #include <SPI.h>
 
-#if defined(__SAM3X8E__)
-    #undef __FlashStringHelper::F(string_literal)
-    #define F(string_literal) string_literal
-#endif
 
-// Option 1: use any pins but a little slower
-Adafruit_ST7735 tft = Adafruit_ST7735(cs, dc, mosi, sclk, rst);
+// For the breakout, you can use any 2 or 3 pins
+// These pins will also work for the 1.8" TFT shield
+#define TFT_CS     10
+#define TFT_RST    9  // you can also connect this to the Arduino reset
+                      // in which case, set this #define pin to 0!
+#define TFT_DC     8
 
-// Option 2: must use the hardware SPI pins
+// Option 1 (recommended): must use the hardware SPI pins
 // (for UNO thats sclk = 13 and sid = 11) and pin 10 must be
 // an output. This is much faster - also required if you want
 // to use the microSD card (see the image drawing example)
-//Adafruit_ST7735 tft = Adafruit_ST7735(cs, dc, rst);
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
+
+// Option 2: use any pins but a little slower!
+#define TFT_SCLK 13   // set these to be whatever pins you like!
+#define TFT_MOSI 11   // set these to be whatever pins you like!
+//Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 
 void setup(void) {
   Serial.begin(9600);
-  Serial.print("hello!");
+  Serial.print("Hello! Adafruit ST7735 rotation test");
 
-  // Our supplier changed the 1.8" display slightly after Jan 10, 2012
-  // so that the alignment of the TFT had to be shifted by a few pixels
-  // this just means the init code is slightly different. Check the
-  // color of the tab to see which init code to try. If the display is
-  // cut off or has extra 'random' pixels on the top & left, try the
-  // other option!
-  // If you are seeing red and green color inversion, use Black Tab
-
-  // If your TFT's plastic wrap has a Black Tab, use the following:
+  // Use this initializer if you're using a 1.8" TFT
   tft.initR(INITR_BLACKTAB);   // initialize a ST7735S chip, black tab
-  // If your TFT's plastic wrap has a Red Tab, use the following:
-  //tft.initR(INITR_REDTAB);   // initialize a ST7735R chip, red tab
-  // If your TFT's plastic wrap has a Green Tab, use the following:
-  //tft.initR(INITR_GREENTAB); // initialize a ST7735R chip, green tab
+
+  // Use this initializer (uncomment) if you're using a 1.44" TFT
+  //tft.initR(INITR_144GREENTAB);   // initialize a ST7735S chip, black tab
 
   Serial.println("init");
 
