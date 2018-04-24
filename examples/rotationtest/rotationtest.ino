407044ca2cbc386b55d6922ee93c78aa2710e32f
ladyada <limor@ladyada.net>
2018-04-24 16:23:18 -0400

    update to use ST7789

--- examples/rotationtest/rotationtest.ino
+++ examples/rotationtest/rotationtest.ino
@@ -22,7 +22,8 @@ as well as Adafruit raw 1.8" TFT display
  ****************************************************/
 
 #include <Adafruit_GFX.h>    // Core graphics library
-#include <Adafruit_ST7735.h> // Hardware-specific library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
 #include <SPI.h>
 
 
@@ -39,14 +40,17 @@ as well as Adafruit raw 1.8" TFT display
 // to use the microSD card (see the image drawing example)
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
 
+// For 1.54" TFT with ST7789
+//Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS,  TFT_DC, TFT_RST);
+
 // Option 2: use any pins but a little slower!
-#define TFT_SCLK 13   // set these to be whatever pins you like!
-#define TFT_MOSI 11   // set these to be whatever pins you like!
+//#define TFT_SCLK 13   // set these to be whatever pins you like!
+//#define TFT_MOSI 11   // set these to be whatever pins you like!
 //Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 
 void setup(void) {
   Serial.begin(9600);
-  Serial.print("Hello! Adafruit ST7735 rotation test");
+  Serial.print("Hello! Adafruit ST77XX rotation test");
 
   // Use this initializer if you're using a 1.8" TFT
   tft.initR(INITR_BLACKTAB);   // initialize a ST7735S chip, black tab
@@ -57,10 +61,13 @@ void setup(void) {
   // Use this initializer (uncomment) if you're using a 0.96" 180x60 TFT
   //tft.initR(INITR_MINI160x80);   // initialize a ST7735S chip, mini display
 
+  // Use this initializer (uncomment) if you're using a 1.54" 240x240 TFT
+  //tft.init(240, 240);   // initialize a ST7789 chip, 240x240 pixels
+  
   Serial.println("init");
 
   tft.setTextWrap(false); // Allow text to run off right edge
-  tft.fillScreen(ST7735_BLACK);
+  tft.fillScreen(ST77XX_BLACK);
 
   Serial.println("This is a test of the rotation capabilities of the TFT library!");
   Serial.println("Press <SEND> (or type a character) to advance");
@@ -85,20 +92,20 @@ void loop(void) {
 
 void rotateText() {
   for (uint8_t i=0; i<4; i++) {
-    tft.fillScreen(ST7735_BLACK);
+    tft.fillScreen(ST77XX_BLACK);
     Serial.println(tft.getRotation(), DEC);
 
     tft.setCursor(0, 30);
-    tft.setTextColor(ST7735_RED);
+    tft.setTextColor(ST77XX_RED);
     tft.setTextSize(1);
     tft.println("Hello World!");
-    tft.setTextColor(ST7735_YELLOW);
+    tft.setTextColor(ST77XX_YELLOW);
     tft.setTextSize(2);
     tft.println("Hello World!");
-    tft.setTextColor(ST7735_GREEN);
+    tft.setTextColor(ST77XX_GREEN);
     tft.setTextSize(3);
     tft.println("Hello World!");
-    tft.setTextColor(ST7735_BLUE);
+    tft.setTextColor(ST77XX_BLUE);
     tft.setTextSize(4);
     tft.print(1234.567);
     while (!Serial.available());
@@ -110,10 +117,10 @@ void rotateText() {
 
 void rotateFillcircle(void) {
   for (uint8_t i=0; i<4; i++) {
-    tft.fillScreen(ST7735_BLACK);
+    tft.fillScreen(ST77XX_BLACK);
     Serial.println(tft.getRotation(), DEC);
 
-    tft.fillCircle(10, 30, 10, ST7735_YELLOW);
+    tft.fillCircle(10, 30, 10, ST77XX_YELLOW);
 
     while (!Serial.available());
     Serial.read();  Serial.read();  Serial.read();
@@ -124,10 +131,10 @@ void rotateFillcircle(void) {
 
 void rotateDrawcircle(void) {
   for (uint8_t i=0; i<4; i++) {
-    tft.fillScreen(ST7735_BLACK);
+    tft.fillScreen(ST77XX_BLACK);
     Serial.println(tft.getRotation(), DEC);
 
-    tft.drawCircle(10, 30, 10, ST7735_YELLOW);
+    tft.drawCircle(10, 30, 10, ST77XX_YELLOW);
  
     while (!Serial.available());
     Serial.read();  Serial.read();  Serial.read();
@@ -138,10 +145,10 @@ void rotateDrawcircle(void) {
 
 void rotateFillrect(void) {
   for (uint8_t i=0; i<4; i++) {
-    tft.fillScreen(ST7735_BLACK);
+    tft.fillScreen(ST77XX_BLACK);
     Serial.println(tft.getRotation(), DEC);
 
-    tft.fillRect(10, 20, 10, 20, ST7735_GREEN);
+    tft.fillRect(10, 20, 10, 20, ST77XX_GREEN);
  
     while (!Serial.available());
     Serial.read();  Serial.read();  Serial.read();
@@ -152,10 +159,10 @@ void rotateFillrect(void) {
 
 void rotateDrawrect(void) {
   for (uint8_t i=0; i<4; i++) {
-    tft.fillScreen(ST7735_BLACK);
+    tft.fillScreen(ST77XX_BLACK);
     Serial.println(tft.getRotation(), DEC);
 
-    tft.drawRect(10, 20, 10, 20, ST7735_GREEN);
+    tft.drawRect(10, 20, 10, 20, ST77XX_GREEN);
  
     while (!Serial.available());
     Serial.read();  Serial.read();  Serial.read();
@@ -166,11 +173,11 @@ void rotateDrawrect(void) {
 
 void rotateFastline(void) {
   for (uint8_t i=0; i<4; i++) {
-    tft.fillScreen(ST7735_BLACK);
+    tft.fillScreen(ST77XX_BLACK);
     Serial.println(tft.getRotation(), DEC);
 
-    tft.drawFastHLine(0, 20, tft.width(), ST7735_RED);
-    tft.drawFastVLine(20, 0, tft.height(), ST7735_BLUE);
+    tft.drawFastHLine(0, 20, tft.width(), ST77XX_RED);
+    tft.drawFastVLine(20, 0, tft.height(), ST77XX_BLUE);
 
     while (!Serial.available());
     Serial.read();  Serial.read();  Serial.read();
@@ -181,10 +188,10 @@ void rotateFastline(void) {
 
 void rotateLine(void) {
   for (uint8_t i=0; i<4; i++) {
-    tft.fillScreen(ST7735_BLACK);
+    tft.fillScreen(ST77XX_BLACK);
     Serial.println(tft.getRotation(), DEC);
 
-    tft.drawLine(tft.width()/2, tft.height()/2, 0, 0, ST7735_RED);
+    tft.drawLine(tft.width()/2, tft.height()/2, 0, 0, ST77XX_RED);
     while (!Serial.available());
     Serial.read();  Serial.read();  Serial.read();
 
@@ -194,10 +201,10 @@ void rotateLine(void) {
 
 void rotatePixel(void) {
   for (uint8_t i=0; i<4; i++) {
-    tft.fillScreen(ST7735_BLACK);
+    tft.fillScreen(ST77XX_BLACK);
     Serial.println(tft.getRotation(), DEC);
 
-    tft.drawPixel(10,20, ST7735_WHITE);
+    tft.drawPixel(10,20, ST77XX_WHITE);
     while (!Serial.available());
     Serial.read();  Serial.read();  Serial.read();
 
@@ -207,10 +214,10 @@ void rotatePixel(void) {
 
 void rotateTriangle(void) {
   for (uint8_t i=0; i<4; i++) {
-    tft.fillScreen(ST7735_BLACK);
+    tft.fillScreen(ST77XX_BLACK);
     Serial.println(tft.getRotation(), DEC);
 
-    tft.drawTriangle(20, 10, 10, 30, 30, 30, ST7735_GREEN);
+    tft.drawTriangle(20, 10, 10, 30, 30, 30, ST77XX_GREEN);
     while (!Serial.available());
     Serial.read();  Serial.read();  Serial.read();
 
@@ -220,10 +227,10 @@ void rotateTriangle(void) {
 
 void rotateFillTriangle(void) {
   for (uint8_t i=0; i<4; i++) {
-    tft.fillScreen(ST7735_BLACK);
+    tft.fillScreen(ST77XX_BLACK);
     Serial.println(tft.getRotation(), DEC);
 
-    tft.fillTriangle(20, 10, 10, 30, 30, 30, ST7735_RED);
+    tft.fillTriangle(20, 10, 10, 30, 30, 30, ST77XX_RED);
     while (!Serial.available());
     Serial.read();  Serial.read();  Serial.read();
 
@@ -233,10 +240,10 @@ void rotateFillTriangle(void) {
 
 void rotateRoundRect(void) {
   for (uint8_t i=0; i<4; i++) {
-    tft.fillScreen(ST7735_BLACK);
+    tft.fillScreen(ST77XX_BLACK);
     Serial.println(tft.getRotation(), DEC);
 
-    tft.drawRoundRect(20, 10, 25, 15, 5, ST7735_BLUE);
+    tft.drawRoundRect(20, 10, 25, 15, 5, ST77XX_BLUE);
     while (!Serial.available());
     Serial.read();  Serial.read();  Serial.read();
 
@@ -246,10 +253,10 @@ void rotateRoundRect(void) {
 
 void rotateFillRoundRect(void) {
   for (uint8_t i=0; i<4; i++) {
-    tft.fillScreen(ST7735_BLACK);
+    tft.fillScreen(ST77XX_BLACK);
     Serial.println(tft.getRotation(), DEC);
 
-    tft.fillRoundRect(20, 10, 25, 15, 5, ST7735_CYAN);
+    tft.fillRoundRect(20, 10, 25, 15, 5, ST77XX_CYAN);
     while (!Serial.available());
     Serial.read();  Serial.read();  Serial.read();
 
@@ -259,10 +266,10 @@ void rotateFillRoundRect(void) {
 
 void rotateChar(void) {
   for (uint8_t i=0; i<4; i++) {
-    tft.fillScreen(ST7735_BLACK);
+    tft.fillScreen(ST77XX_BLACK);
     Serial.println(tft.getRotation(), DEC);
 
-    tft.drawChar(25, 15, 'A', ST7735_WHITE, ST7735_WHITE, 1);
+    tft.drawChar(25, 15, 'A', ST77XX_WHITE, ST77XX_WHITE, 1);
     while (!Serial.available());
     Serial.read();  Serial.read();  Serial.read();
 
@@ -272,12 +279,12 @@ void rotateChar(void) {
 
 void rotateString(void) {
   for (uint8_t i=0; i<4; i++) {
-    tft.fillScreen(ST7735_BLACK);
+    tft.fillScreen(ST77XX_BLACK);
     Serial.println(tft.getRotation(), DEC);
 
     tft.setCursor(8, 25);
     tft.setTextSize(1);
-    tft.setTextColor(ST7735_WHITE);
+    tft.setTextColor(ST77XX_WHITE);
     tft.print("Adafruit Industries");
     while (!Serial.available());
     Serial.read();  Serial.read();  Serial.read();
