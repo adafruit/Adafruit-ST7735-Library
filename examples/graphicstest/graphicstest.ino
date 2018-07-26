4b6bd157728056f15d9fd3b064c31c868d88883a
Shawn A <spam@rumental.com>
2018-07-26 15:29:28 -0500

    fixes #48
    adds some delays in testlines to feed esp8266 wdt

--- examples/graphicstest/graphicstest.ino
+++ examples/graphicstest/graphicstest.ino
@@ -129,38 +129,45 @@ void loop() {
   tft.invertDisplay(false);
   delay(500);
 }
-
 void testlines(uint16_t color) {
   tft.fillScreen(ST77XX_BLACK);
   for (int16_t x=0; x < tft.width(); x+=6) {
     tft.drawLine(0, 0, x, tft.height()-1, color);
+    delay(0);
   }
   for (int16_t y=0; y < tft.height(); y+=6) {
     tft.drawLine(0, 0, tft.width()-1, y, color);
+    delay(0);
   }
 
   tft.fillScreen(ST77XX_BLACK);
   for (int16_t x=0; x < tft.width(); x+=6) {
     tft.drawLine(tft.width()-1, 0, x, tft.height()-1, color);
+    delay(0);
   }
   for (int16_t y=0; y < tft.height(); y+=6) {
     tft.drawLine(tft.width()-1, 0, 0, y, color);
+    delay(0);
   }
 
   tft.fillScreen(ST77XX_BLACK);
   for (int16_t x=0; x < tft.width(); x+=6) {
     tft.drawLine(0, tft.height()-1, x, 0, color);
+    delay(0);
   }
   for (int16_t y=0; y < tft.height(); y+=6) {
     tft.drawLine(0, tft.height()-1, tft.width()-1, y, color);
+    delay(0);
   }
 
   tft.fillScreen(ST77XX_BLACK);
   for (int16_t x=0; x < tft.width(); x+=6) {
     tft.drawLine(tft.width()-1, tft.height()-1, x, 0, color);
+    delay(0);
   }
   for (int16_t y=0; y < tft.height(); y+=6) {
     tft.drawLine(tft.width()-1, tft.height()-1, 0, y, color);
+    delay(0);
   }
 }
 
