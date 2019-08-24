715fb29ab142070910409ab25367ceb4f039d7d0
Travis CI <travis@travis-ci.org>
2019-08-24 20:23:59 +0000

    Deploy code docs to GitHub Pages Travis build: 88
    Commit: 3b65701918a263ed6bb2a8e946f21a4592c97236

--- html/search/functions_4.js
+++ html/search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['enabledisplay',['enableDisplay',['../class_adafruit___s_t77xx.html#ad5ba3ceabd970c59c5a40c0b34b4dfbd',1,'Adafruit_ST77xx']]]
+  ['enabledisplay',['enableDisplay',['../class_adafruit___s_t77xx.html#ad5ba3ceabd970c59c5a40c0b34b4dfbd',1,'Adafruit_ST77xx']]],
+  ['enabletearing',['enableTearing',['../class_adafruit___s_t77xx.html#ac58a3e36caf4d5c30cb17c91a57e074c',1,'Adafruit_ST77xx']]]
 ];
