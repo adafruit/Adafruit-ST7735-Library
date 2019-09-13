a396f3e85a7491c0ef3e227be4c238e78baaa994
Melissa LeBlanc-Williams <melissa@adafruit.com>
2019-09-13 10:42:34 -0700

    Split out examples for boards with integrated displays

--- examples/graphicstest_pybadge_pygamer/graphicstest_pybadge_pygamer.ino
+++ examples/graphicstest_pybadge_pygamer/graphicstest_pybadge_pygamer.ino
@@ -1,24 +1,13 @@
 /**************************************************************************
   This is a library for several Adafruit displays based on ST77* drivers.
 
-  Works with the Adafruit 1.8" TFT Breakout w/SD card
-    ----> http://www.adafruit.com/products/358
-  The 1.8" TFT shield
-    ----> https://www.adafruit.com/product/802
-  The 1.44" TFT breakout
-    ----> https://www.adafruit.com/product/2088
-  The 1.3" TFT breakout
-  ----> https://www.adafruit.com/product/4313
-  The 1.54" TFT breakout
-    ----> https://www.adafruit.com/product/3787
-  The 2.0" TFT breakout
-    ----> https://www.adafruit.com/product/4311
-  as well as Adafruit raw 1.8" TFT display
-    ----> http://www.adafruit.com/products/618
+  Works with the following products with integrated displays
+  The PyBadge
+    ----> http://www.adafruit.com/products/4200
+  The PyGamer
+    ----> http://www.adafruit.com/products/4242
 
   Check out the links above for our tutorials and wiring diagrams.
-  These displays use SPI to communicate, 4 or 5 pins are required to
-  interface (RST is optional).
 
   Adafruit invests time and resources providing this open source code,
   please support Adafruit and open-source hardware by purchasing
@@ -30,60 +19,22 @@
 
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
-#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
 #include <SPI.h>
 
-#if defined(ESP32)
-  #define TFT_CS         5
-  #define TFT_RST        22 
-  #define TFT_DC         21
-  //
-  // define not needed for all pins; reference for ESP32 physical pins connections to VSPI:
-  // SDA  GPIO23 aka VSPI MOSI
-  // SCLK GPIO18 aka SCK aka VSPI SCK
-  // D/C  GPIO21 aka A0 (also I2C SDA)
-  // RST  GPIO22 aka RESET (also I2C SCL)
-  // CS   GPIO5  aka chip select
-  // LED  3.3V
-  // VCC  5V
-  // GND - GND
-  //
-#elif defined(ESP8266)
-  #define TFT_CS         4
-  #define TFT_RST        16                                            
-  #define TFT_DC         5
-
-#else
-  // For the breakout board, you can use any 2 or 3 pins.
-  // These pins will also work for the 1.8" TFT shield.
-  #define TFT_CS        10
-  #define TFT_RST        9 // Or set to -1 and connect to Arduino RESET pin
-  #define TFT_DC         8
-#endif
+#define TFT_CS        44 // PyBadge/PyGamer display control pins: chip select
+#define TFT_RST       46 // Display reset
+#define TFT_DC        45 // Display data/command select
+#define TFT_BACKLIGHT 47 // Display backlight pin
 
 // OPTION 1 (recommended) is to use the HARDWARE SPI pins, which are unique
-// to each board and not reassignable. For Arduino Uno: MOSI = pin 11 and
-// SCLK = pin 13. This is the fastest mode of operation and is required if
-// using the breakout board's microSD card.
-
-// For 1.44" and 1.8" TFT with ST7735 use:
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
-
-// For 1.3", 1.54", and 2.0" TFT with ST7789:
-//Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
-
+// to each board and not reassignable.
+Adafruit_ST7735 tft = Adafruit_ST7735(&SPI1, TFT_CS, TFT_DC, TFT_RST);
 
 // OPTION 2 lets you interface the display using ANY TWO or THREE PINS,
 // tradeoff being that performance is not as fast as hardware SPI above.
-//#define TFT_MOSI 11  // Data out
-//#define TFT_SCLK 13  // Clock out
-
-// For ST7735-based displays, we will use this call
+//#define TFT_MOSI 41  // Data out
+//#define TFT_SCLK 42  // Clock out
 //Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
-
-// OR for the ST7789-based displays, we will use this call
-//Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
-
 
 float p = 3.1415926;
 
@@ -91,20 +42,10 @@ void setup(void) {
   Serial.begin(9600);
   Serial.print(F("Hello! ST77xx TFT Test"));
 
-  // Use this initializer if using a 1.8" TFT screen:
-  tft.initR(INITR_BLACKTAB);      // Init ST7735S chip, black tab
-
-  // OR use this initializer (uncomment) if using a 1.44" TFT:
-  //tft.initR(INITR_144GREENTAB); // Init ST7735R chip, green tab
-
-  // OR use this initializer (uncomment) if using a 0.96" 180x60 TFT:
-  //tft.initR(INITR_MINI160x80);  // Init ST7735S mini display
-
-  // OR use this initializer (uncomment) if using a 1.3" or 1.54" 240x240 TFT:
-  //tft.init(240, 240);           // Init ST7789 240x240
-
-  // OR use this initializer (uncomment) if using a 2.0" 320x240 TFT:
-  //tft.init(240, 320);           // Init ST7789 320x240
+  tft.initR(INITR_BLACKTAB);        // Initialize ST7735R screen
+  tft.setRotation(1);
+  pinMode(TFT_BACKLIGHT, OUTPUT);
+  digitalWrite(TFT_BACKLIGHT, HIGH); // Backlight on
 
   Serial.println(F("Initialized"));
 
