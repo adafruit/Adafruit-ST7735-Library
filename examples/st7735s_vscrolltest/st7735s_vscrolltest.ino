d415a73fed877ea652cdbc88aca1ca5a4db239f5
Neku <nekuneko@users.noreply.github.com>
2020-07-06 21:41:21 +0200

    Update st7735s_vscrolltest.ino
    Changes to match esp8266 needs

--- examples/st7735s_vscrolltest/st7735s_vscrolltest.ino
+++ examples/st7735s_vscrolltest/st7735s_vscrolltest.ino
@@ -343,9 +343,9 @@ void vscrolltest ()
 
 void hscrolltest() 
 {
-  char message [] PROGMEM = "In a village of La Mancha, the name of which I have no\
-  desire to call to mind, there lived not long since one of those gentlemen that keep\
-  a lance in the lance-rack, an old buckler, a lean hack, and a greyhound for coursing.";
+  char message [] = "In a village of La Mancha, the name of which I have no desire\
+  to call to mind, there lived not long since one of those gentlemen that keep a lance\
+  in the lance-rack, an old buckler, a lean hack, and a greyhound for coursing.";
   int x, minX;
 
   tft.setRotation(1);
