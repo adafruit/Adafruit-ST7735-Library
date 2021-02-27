518ab5e05d61f5f5249345268c38b8ec65df8c47
Doxygen CI <ci-arduino@invalid>
2021-02-27 01:14:14 +0000

    Deploy docs to GitHub Pages from commit 8695fb8e2d
    Commit: 8695fb8e2d0266a8c7e1b8862cf44c9be575adcf
    GitHub Actions run: 604451136

--- html/search/variables_0.js
+++ html/search/variables_0.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['_5fcolstart',['_colstart',['../class_adafruit___s_t77xx.html#ad88f0a92e6d38be19041e793a8b400ea',1,'Adafruit_ST77xx']]],
-  ['_5frowstart',['_rowstart',['../class_adafruit___s_t77xx.html#a0b93050a888d6c1d3ce86abfcdf47d33',1,'Adafruit_ST77xx']]]
+  ['_5fcolstart2',['_colstart2',['../class_adafruit___s_t7789.html#aa786364a8352e6fe9261c7b198ea7d6d',1,'Adafruit_ST7789']]],
+  ['_5frowstart',['_rowstart',['../class_adafruit___s_t77xx.html#a0b93050a888d6c1d3ce86abfcdf47d33',1,'Adafruit_ST77xx']]],
+  ['_5frowstart2',['_rowstart2',['../class_adafruit___s_t7789.html#adf85ba74688042c79c1801db0927ad0d',1,'Adafruit_ST7789']]]
 ];
