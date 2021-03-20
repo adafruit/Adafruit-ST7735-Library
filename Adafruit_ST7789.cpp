2de13766b51a2b285d18150ce4903ae7dc0f9a9d
lady ada <limor@ladyada.net>
2021-03-20 18:03:00 -0400

    add 240x280 display

--- Adafruit_ST7789.cpp
+++ Adafruit_ST7789.cpp
@@ -108,6 +108,12 @@ void Adafruit_ST7789::init(uint16_t width, uint16_t height, uint8_t mode) {
     _rowstart = _rowstart2 = (int)((320 - height + 1) / 2);
     _colstart = (int)((240 - width + 1) / 2);
     _colstart2 = (int)((240 - width) / 2);
+  }
+  if (width == 240 && height == 280) {
+    // 1.69" display
+    _rowstart = 20;
+    _rowstart2 = 0;
+    _colstart = _colstart2 = 0;
   } else {
     // 1.3", 1.54", and 2.0" displays
     _rowstart = (320 - height);
