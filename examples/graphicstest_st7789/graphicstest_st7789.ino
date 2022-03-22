50d22b9757a200ebe32bcca7cf8d45b6bad0d4fd
Melissa LeBlanc-Williams <melissa@adafruit.com>
2022-03-22 10:26:39 -0700

    Remove rotation from example

--- examples/graphicstest_st7789/graphicstest_st7789.ino
+++ examples/graphicstest_st7789/graphicstest_st7789.ino
@@ -94,7 +94,6 @@ void setup(void) {
   // Note that speed allowable depends on chip and quality of wiring, if you go too fast, you
   // may end up with a black screen some times, or all the time.
   //tft.setSPISpeed(40000000);
-  tft.setRotation(0);
   Serial.println(F("Initialized"));
 
   uint16_t time = millis();
